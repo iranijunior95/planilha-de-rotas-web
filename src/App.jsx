a342240c01184b4c03ca38035a4769ef5d655279
Irani Junior <iranijunior95@gmail.com>
2026-01-05 19:10:50 -0300

    Criação e configuração de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+const Home = lazy(() => import("./views/Home"));
+const Employees = lazy(() => import("./views/Employees"));
+const Login = lazy(() => import("./views/Login"));
+const Register = lazy(() => import("./views/Register"));
+const Reports = lazy(() => import("./views/Reports"));
+const Trips = lazy(() => import("./views/Trips"));
+const Vehicles = lazy(() => import("./views/Vehicles"));
+
 export default function App() {
   return (
-    <>planilha de rotas web</>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/funcionarios" element={<Employees />} />
+        <Route path="/veiculos" element={<Vehicles />} />
+        <Route path="/viagens" element={<Trips />} />
+        <Route path="/relatorios" element={<Reports />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
